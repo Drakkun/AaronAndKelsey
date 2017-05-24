f89e75fe2cfa4c82dc5886646b88431896e53524
Sam Martinez <sam.martinez.dev@gmail.com>
2017-05-23 19:47:16 -0500

    Renames directories

--- Pages/index.php
+++ Pages/index.php
@@ -11,9 +11,8 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
     <!-- Project style sheet -->
-    <link rel="stylesheet" href="../CSS/styles.css">
+    <link rel="stylesheet" href="../Styles/styles.css">
 </head>
 <body>
-
 </body>
 </html>
